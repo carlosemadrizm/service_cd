20983d8607c019be3b2cd2f614c8acae621d3311
carlosemadrizm <madrizcv15@gmail.com>
2025-05-24 13:29:21 -0300

    Agregando carpeta login google al repositorio A como subcarpeta

---
+++ login_google/users.sql
@@ -0,0 +1,9 @@
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    google_id VARCHAR(50) NOT NULL UNIQUE,
+    name VARCHAR(100),
+    email VARCHAR(100) UNIQUE,
+    picture TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
